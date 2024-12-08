68b38af2b8b06a983d0bb8a593941840b70613a4
Adam Zhang <898544@lcps.org>
2024-12-08 17:08:03 -0500

    feat(nix): add vsce

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 {pkgs ? import <nixpkgs> {}}:
 with pkgs;
   mkShell {
-    buildInputs = [nodejs typescript-language-server vscodium];
+    buildInputs = [nodejs typescript-language-server vsce vscodium];
   }

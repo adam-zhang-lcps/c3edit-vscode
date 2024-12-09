200c3f489db025fcdbdee1f6d213f3ca41e77030
Adam Zhang <898544@lcps.org>
2024-12-08 19:51:12 -0500

    refactor: remove useless code from template

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as handlers from "./handlers";
 import state from "./state";
 import * as commands from "./commands";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "c3edit" is now active!');
-
 	// Commands
 	context.subscriptions.push(
 		vscode.commands.registerCommand("c3edit.runBackend", commands.runBackend),
